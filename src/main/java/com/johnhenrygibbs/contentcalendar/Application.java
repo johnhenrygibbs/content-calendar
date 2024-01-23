5c2e62620b6500bd1d799b41334a8d4a0d4874b5
John Gibbs <hello@johnhenrygibbs.com>
2024-01-22 16:41:09 -0800

    Spring Core

--- src/main/java/com/johnhenrygibbs/contentcalendar/Application.java
+++ src/main/java/com/johnhenrygibbs/contentcalendar/Application.java
@@ -2,12 +2,18 @@ package com.johnhenrygibbs.contentcalendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate");
+		System.out.println(restTemplate);
 	}
 
 }
