6bddc278aea28f2a4873ef90379dc1c9c185aa18
John Gibbs <hello@johnhenrygibbs.com>
2024-02-03 16:07:32 -0800

    Spring MVC

--- src/main/java/com/johnhenrygibbs/contentcalendar/controller/ContentController.java
+++ src/main/java/com/johnhenrygibbs/contentcalendar/controller/ContentController.java
@@ -2,10 +2,10 @@ package com.johnhenrygibbs.contentcalendar.controller;
 
 import com.johnhenrygibbs.contentcalendar.model.Content;
 import com.johnhenrygibbs.contentcalendar.repository.ContentCollectionRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,8 +26,29 @@ public class ContentController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Content> findById(@PathVariable Integer id) {
-        return repository.findById(id);
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@Valid @RequestBody Content content) {
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.delete(id);
     }
 
 }
